9220635ffbab79a4298a186efdbb0f3d19f51b17
dwune <dwune@naver.com>
2026-04-29 13:49:38 +0900

    Fix: submodule issue and integrate First project

---
+++ First/src/hello/Add.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Add {
+    public static void main(String[] args) {
+        int x; // 첫번째 정수를 저장할 변수
+        int y; // 두번째 정수를 저장할 변수
+        int sum; // 두 정수의 합을 저장하는 변수
+
+        x = 100;
+        y = 200;
+
+        sum = x+y;
+
+        System.out.println("두수의 합 : " + sum);
+    }
+}
