02c5e7fef07e46936368258d57b96eb6e2efcef5
dwune08 <dwune159@gmail.com>
2026-05-12 17:20:10 +0900

    Java study day 8 Multi-dimensionalArray Examples - Argument

--- ArrayExample/src/example/ArrayExample1.java
+++ ArrayExample/src/example/ArrayExample1.java
@@ -10,10 +10,10 @@ public class ArrayExample1 {
         String numbers = input.nextLine();
         String[] array = numbers.split(" ");
         for (String s : array) {
-            num[Integer.parseInt(s) - 1]++;
+            num[Integer.parseInt(s) - 1]++; // 입력받은 수들을 인덱스로 사용하여 배열에 집계
         }
         for(int i = 0; i < num.length; i++) {
-            System.out.println((i+1) + "의 개수 : " + num[i]);
+            System.out.println((i+1) + "의 개수 : " + num[i]); // 출력
         }
         input.close();
     }
