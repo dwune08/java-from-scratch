867fea3fd841d2dbfbc0f0ce73bd5f516ba34fad
dwune08 <dwune@naver.com>
2026-05-15 21:39:17 +0900

    day 11 homework - Class example homework

--- JavaClassPractice/src/exam_class/SmartPhone.java
+++ JavaClassPractice/src/exam_class/SmartPhone.java
@@ -1,4 +1,57 @@
 package exam_class;
 
 public class SmartPhone {
+    private String maker;
+    private String name;
+    private int price;
+    private int discountRate;
+
+    public SmartPhone() { }
+
+    public SmartPhone(String maker, String name, int price) {
+        this(maker, name, price, 0);
+    }
+
+    public int calculateDiscount() {
+        return (int)(price*(100-discountRate)/100);
+    }
+
+    public SmartPhone(String maker, String name, int price, int discountRate) {
+        this.maker = maker;
+        this.name = name;
+        this.price = price;
+        this.discountRate = discountRate;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(int discountRate) {
+        this.discountRate = discountRate;
+    }
 }
--- JavaClassPractice/src/exam_class/SmartPhoneMain.java
+++ JavaClassPractice/src/exam_class/SmartPhoneMain.java
@@ -1,4 +1,25 @@
 package exam_class;
 
 public class SmartPhoneMain {
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------");
+
+        SmartPhone sp1 = new SmartPhone("삼성", "갤럭시 S25 엣지", 1639000);
+        PhonePrint(sp1);
+        SmartPhone sp2 = new SmartPhone("애플", "iPhone 17", 1250000, 10);
+        PhonePrint(sp2);
+        SmartPhone sp3 = new SmartPhone("삼성", "갤럭시 Z 플립7", 1485000, 5);
+        PhonePrint(sp3);
+
+    }
+
+    public static void PhonePrint(SmartPhone sp) {
+        System.out.println(sp.getName() + " [" + sp.getMaker() + "]");
+        System.out.printf("가격 : %,d원\n", sp.getPrice());
+        if(sp.getDiscountRate() != 0) {
+            System.out.printf("할인가격(%d%%) : %,d원\n", sp.getDiscountRate(), sp.calculateDiscount());
+        }
+        System.out.println("---------------------------");
+    }
 }
